4c8ea5fe50afdc6c272870320f493bf90d032db8
Jefferson Lam <jeffersonwlam@gmail.com>
2018-07-06 02:10:42 -0500

    Add src and dist folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,33 @@
-import gulp from 'gulp';
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', ['sass', 'sass:watch', 'html', 'browser-sync']);
+
+gulp.task('sass', function () {
+  return gulp.src('./src/styles/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(reload({stream:true}))
+    .pipe(gulp.dest('./dist/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/styles/**/*.scss', ['sass']);
+});
+
+gulp.task('html', function() {
+  gulp.src('./*.html')
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+      server: {
+          baseDir: 'dist/'
+      }
+  });
+});
