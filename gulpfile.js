d2a0843078f32f801b17d1b39b5053244791d897
Jefferson Lam <jeffersonwlam@gmail.com>
2018-07-08 20:54:08 -0500

    Add maps task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-gulp.task('default', ['sass', 'sass:watch', 'html', 'html:watch', 'browser-sync']);
+gulp.task('default', ['sass', 'sass:watch', 'html', 'html:watch', 'browser-sync', 'maps']);
 
 gulp.task('sass', function () {
   return gulp.src('./src/styles/**/*.scss')
@@ -26,6 +26,12 @@ gulp.task('html', function() {
 
 gulp.task('html:watch', function() {
   gulp.watch('./*.html', ['html']);
+});
+
+// Needed to ensure /dist/maps files exist
+gulp.task('maps', function(){
+  gulp.src('maps/*')
+    .pipe(gulp.dest('dist/maps'))
 });
 
 gulp.task('browser-sync', function() {
