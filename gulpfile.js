5acae68004cda739c7a214d6bff70923d3969ad4
Jefferson Lam <jeffersonwlam@gmail.com>
2018-10-03 22:58:23 -0500

    Consolidate watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,13 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-gulp.task('default', ['sass', 'sass:watch', 'js', 'js:watch', 'html', 'html:watch', 'browser-sync',]);
+gulp.task('default', ['sass', 'js', 'html', 'browser-sync', 'watch']);
+
+gulp.task('watch', function() {
+  gulp.watch('./src/styles/**/*.scss', ['sass']);
+  gulp.watch('./*.html', ['html']);
+  gulp.watch('./src/js/*.js', ['js']);
+});
 
 gulp.task('sass', function () {
   return gulp.src('./src/styles/**/*.scss')
@@ -14,29 +20,16 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/styles/**/*.scss', ['sass']);
-});
-
 gulp.task('html', function() {
   gulp.src('./*.html')
     .pipe(gulp.dest('dist/'))
     .pipe(reload({stream:true}));
 });
 
-gulp.task('html:watch', function() {
-  gulp.watch('./*.html', ['html']);
-});
-
-
 gulp.task('js', function() {
   gulp.src('./src/js/*.js')
     .pipe(reload({stream:true}))
     .pipe(gulp.dest('dist'));
-});
-
-gulp.task('js:watch', function() {
-  gulp.watch('./src/js/*.js', ['js']);
 });
 
 gulp.task('browser-sync', function() {
