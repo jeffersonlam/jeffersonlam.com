610171a2158d393f8c03ed33df4394f80db54b29
Jefferson Lam <jeffersonwlam@gmail.com>
2018-10-16 21:37:06 -0500

    Migrate to gulp4; add build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,113 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+const { task, src, dest, parallel, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
-gulp.task('default', ['sass', 'js', 'html', 'assets', 'browser-sync', 'watch']);
+// Build & Deploy
+const del = require('del');
+const rename = require('gulp-rename');
+const csso = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const ghpages = require('gulp-gh-pages');
 
-gulp.task('watch', function() {
-  gulp.watch('./src/styles/**/*.scss', ['sass']);
-  gulp.watch('./*.html', ['html']);
-  gulp.watch('./src/js/*.js', ['js']);
-});
-
-gulp.task('sass', function () {
-  return gulp.src('./src/styles/**/*.scss')
+task('styles', () => {
+  return src('./src/styles/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(reload({stream:true}))
-    .pipe(gulp.dest('./dist'));
+    .pipe(dest('./dist'));
 });
 
-gulp.task('html', function() {
-  gulp.src('./*.html')
-    .pipe(gulp.dest('dist/'))
-    .pipe(reload({stream:true}));
+task('styles:build', () => {
+  return src('./src/styles/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(csso())
+    .pipe(rename('styles.css'))
+    .pipe(dest('./build'));
 });
 
-gulp.task('js', function() {
-  gulp.src('./src/js/*.js')
+task('html', () => {
+  return src('./src/**/*.html')
     .pipe(reload({stream:true}))
-    .pipe(gulp.dest('dist'));
+    .pipe(dest('./dist'));
 });
 
-gulp.task('assets', function() {
-  gulp.src('./src/assets/*')
+task('html:build', () => {
+  return src('./src/**/*.html')
+    .pipe(dest('./build'));
+});
+
+task('watch', () => {
+  watch('./src/**/*.html', parallel('html', d => d()));
+  watch('./src/styles/**/*.scss', parallel('styles', d => d()));
+  watch('./src/js/**/*.js', parallel('js', d => d()));
+});
+
+task('js', () => {
+  return src('./src/js/*.js')
     .pipe(reload({stream:true}))
-    .pipe(gulp.dest('dist/'));
+    .pipe(dest('dist'));
 });
 
-gulp.task('browser-sync', function() {
+task('js:build', () => {
+  return src('./src/js/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(dest('build'));
+});
+
+task('bs', () => {
   browserSync({
-    open: false,
-    notify: false,
     server: {
-      baseDir: 'dist/'
+        baseDir: 'dist/'
     },
+    open: false,
+    notify: false
   });
 });
+
+task('bs:build', () => {
+  browserSync({
+    server: {
+        baseDir: 'build/'
+    },
+    open: false,
+    notify: false
+  });
+});
+
+task('clean', () => {
+  return del(['build/**/*']);
+});
+
+task('ghpages', function () {
+  return src("./build/**/*")
+    .pipe(ghpages())
+});
+
+task('default',
+  series(
+    parallel('html', 'styles', 'js'),
+    parallel('bs', 'watch')
+  )
+);
+task('build',
+  series(
+    'clean',
+    parallel('styles:build', 'html:build', 'js:build'),
+    'bs:build'
+  )
+);
